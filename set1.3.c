86303200925f91197fee9c9066c453c24526925d
rohininair98 <47473608+rohininair98@users.noreply.github.com>
2019-02-17 11:17:58 +0530

    Create set1.3.c

---
+++ set1.3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,remainder,reverse=0;
+int result;
+scanf("%d",&n);
+while(n!=0)
+{
+remainder=n%10;
+reverse=reverse*10+remainder;
+n=n/10;
+}
+printf("\n%d",reverse);
+    return 0;
+}
