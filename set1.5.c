6d594fe517fb4e714184337e366149ea0a5a4b46
rohininair98 <47473608+rohininair98@users.noreply.github.com>
2019-02-17 11:23:35 +0530

    Create set1.5.c

---
+++ set1.5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[100];
+        int p=0 , i,v,x;
+
+    scanf("%s",a);
+   
+    for(i=0 ; i < a[i] != '\0' ; i++)
+{
+	if(a[i] == 'i')
+{
+	p= p+1;
+}
+else if( a[i] == 'v') 
+{
+	p=p+5;
+}
+	else if( a[i] == 'x')
+{
+		p= p + 10;	
+	}
+}
+ for(i=0 ; i < a[i] != '\0' ; i++)
+{
+	if(a[i] == 'i' && (a[i+1] == 'v' ||   a[i+1]== 'x'))
+	p = p-2;
+	}
+	   
+	printf("%d\n",p);
+    return 0;
+}
